aadb855df3839b702b1e9f91152547811fc0c036
fxparoo <akwuziefunds@gmail.com>
2022-06-30 13:33:46 +0100

    creating model and database postgresql

--- photo/settings.py
+++ photo/settings.py
@@ -81,10 +81,7 @@ WSGI_APPLICATION = 'photo.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 # Password validation
--- photoapp/models.py
+++ photoapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class PhotoRepo(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    profile = models.ImageField(null=True, blank=True, default='default.jpg')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_date']
